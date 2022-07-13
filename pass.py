c6e54394dab9226048bf256bd2b4211841247560
Raga-M <107785840+Raga-M@users.noreply.github.com>
2022-07-13 13:04:53 +0530

    Delete pass.py

--- pass.py
+++
@@ -1,9 +0,0 @@
-import random
-lower="abcdefghijklmnopqrstuvwxyz"
-upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-num="0123456789"
-symbol="!@#$%^&*_~"
-string=lower+upper+num+symbol
-length=8
-password="".join(random.sample(string,length))
-print("Password :"+password)
